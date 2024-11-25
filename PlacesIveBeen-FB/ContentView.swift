bfdf42d221e8fcb2288e54cbce14943ef458a748
gallaugher <john.gallaugher@bc.edu>
2024-11-25 16:34:09 -0500

    Initial Commit

---
+++ PlacesIveBeen-FB/ContentView.swift
@@ -0,0 +1,21 @@
+// File: ContentView.swift Project: PlacesIveBeen-FB
+// Created by: Prof. John Gallaugher on 11/25/24
+// YouTube.com/profgallaugher  -  threads.net/john.gallaugher
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PlacesIveBeen-FB/PlacesIveBeen_FBApp.swift
@@ -0,0 +1,14 @@
+// File: PlacesIveBeen_FBApp.swift Project: PlacesIveBeen-FB
+// Created by: Prof. John Gallaugher on 11/25/24
+// YouTube.com/profgallaugher  -  threads.net/john.gallaugher
+
+import SwiftUI
+
+@main
+struct PlacesIveBeen_FBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
